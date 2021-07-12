14b7815aa77840fa8b7b8554cfdec783ea30856b
zachkirkrey <zachkirkrey@gmail.com>
2021-07-12 12:42:40 +0000

    oc report entity

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,9 +1,12 @@
 # flake8: noqa F401
 from .auth import Login
 from .project import Project, ProjectCreate
+from .oc_report import OCReport, OCReportExport
 
 ENDPOINTS_MAP = {
     Login: "/auth",
     Project: "/project/<project_id>",
     ProjectCreate: "/project",
+    OCReport: "/oc-report",
+    OCReportExport: "/oc-report/export",
 }
