bee59306696364a593cb83540457a0701f5170d5
zachkirkrey <zachkirkrey@gmail.com>
2021-07-12 11:37:31 +0000

    create daily-log entity

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,3 +2,4 @@
 from .project import ProjectSchema, ProjectIdPathSchema
 from .base import ErrorSchema, SuccessSchema
 from .auth import UserLoginSchema, AccessTokenResponseSchema, UserDataSchema
+from .daily_log import DailyLogSchema
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,9 +1,11 @@
 # flake8: noqa F401
 from .auth import Login
 from .project import Project, ProjectCreate
+from .daily_log import DailyLog
 
 ENDPOINTS_MAP = {
     Login: "/auth",
     Project: "/project/<project_id>",
     ProjectCreate: "/project",
+    DailyLog: "/daily-log",
 }
