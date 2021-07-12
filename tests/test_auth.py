9fd81bdbfd8af942f7d8f5fa5dfc1a88b9186a0d
nixissmtp <gleb@simple2b.(none)>
2021-07-12 18:15:58 +0000

    adding tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,6 @@
 import pytest
 
 from app import db, create_app
-from tests.utils import register, login, logout
 
 
 @pytest.fixture
@@ -20,42 +19,12 @@ def client():
         app_ctx.pop()
 
 
-def test_auth_pages(client):
-    response = client.get("/register")
-    assert response.status_code == 200
-    response = client.get("/login")
-    assert response.status_code == 200
-    response = client.get("/logout")
-    assert response.status_code == 302
+def test_db_create_user(client):
+    from app.models import User
+    username, passwd = "imgroot", "1234"
+    user = User(username=username, password=passwd, email="imgroot@gmail.com")
+    user.save()
 
-
-def test_register(client):
-    response = client.post(
-        "/register",
-        data=dict(
-            username="sam",
-            email="sam@test.com",
-            password="password",
-            password_confirmation="password",
-        ),
-        follow_redirects=True,
-    )
-    assert b"Registration successful. You are logged in." in response.data
-
-
-def test_login_and_logout(client):
-    # Access to logout view before login should fail.
-    response = logout(client)
-    assert b"Please log in to access this page." in response.data
-    register("sam")
-    response = login(client, "sam")
-    assert b"Login successful." in response.data
-    # Should successfully logout the currently logged in user.
-    response = logout(client)
-    assert b"You were logged out." in response.data
-    # Incorrect login credentials should fail.
-    response = login(client, "sam", "wrongpassword")
-    assert b"Wrong user ID or password." in response.data
-    # Correct credentials should login
-    response = login(client, "sam")
-    assert b"Login successful." in response.data
+    user = User.query.filter(User.username == username).first()
+    assert user
+    assert user.username == username
