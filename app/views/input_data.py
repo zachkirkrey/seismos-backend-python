c32740d368e9a0db2cf57abe42278d5d2f10389f
nixissmtp <gleb@simple2b.(none)>
2021-07-12 14:25:45 +0000

    fix black

--- app/views/input_data.py
+++ app/views/input_data.py
@@ -9,54 +9,49 @@ from app.schemas import InputFileSchema
 
 class InputData(Resource):
     @jwt_required()
-    @flasgger.swag_from({
-        'parameters': flasgger.marshmallow_apispec.schema2parameters(
-            InputFileSchema,
-            location='form'
-        ),
-        'requestBody': {
-            'required': True,
-            'content': {
-                'multipart/form-data': {
-                    'schema': flasgger.marshmallow_apispec.schema2jsonschema(
-                        marshmallow.Schema.from_dict({
-                            **InputFileSchema().fields,
-                            'attachments': marshmallow.fields.List(
-                                marshmallow.fields.Raw(
-                                    metadata=dict(
-                                        type='file',
-                                        description='files to attach'
-                                    )
-                                )
+    @flasgger.swag_from(
+        {
+            "parameters": flasgger.marshmallow_apispec.schema2parameters(
+                InputFileSchema, location="form"
+            ),
+            "requestBody": {
+                "required": True,
+                "content": {
+                    "multipart/form-data": {
+                        "schema": flasgger.marshmallow_apispec.schema2jsonschema(
+                            marshmallow.Schema.from_dict(
+                                {
+                                    **InputFileSchema().fields,
+                                    "attachments": marshmallow.fields.List(
+                                        marshmallow.fields.Raw(
+                                            metadata=dict(
+                                                type="file",
+                                                description="files to attach",
+                                            )
+                                        )
+                                    ),
+                                }
                             )
-                        })
-                    ),
-                    'encoding': {
-                        'watchers': {
-                            'style': 'form',
-                            'explode': True
+                        ),
+                        "encoding": {
+                            "watchers": {"style": "form", "explode": True},
+                            "attachments": {"style": "form", "explode": True},
                         },
-                        'attachments': {
-                            'style': 'form',
-                            'explode': True
-                        }
-                    },
-                }
-            }
-        },
-        'responses': {
-            204: {'msg': 'File is uploaded'},
+                    }
+                },
+            },
+            "responses": {
+                204: {"msg": "File is uploaded"},
+            },
         }
-    })
+    )
     def post(self):
         f = request.files["file"]
-        f.save(f'static/{f.filename}')
+        f.save(f"static/{f.filename}")
         return {"msg": "OK"}
 
     @jwt_required()
-    @swagger_decorator(
-        response_schema={200: InputFileSchema}
-    )
+    @swagger_decorator(response_schema={200: InputFileSchema})
     def get(self):
-        """ Get file wich has been uploaded earler """
+        """Get file wich has been uploaded earler"""
         return {"filename": "some file"}
