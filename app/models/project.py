c85e0b1b42200239252446146b5cc82665b7b0f6
nixissmtp <gleb@simple2b.(none)>
2021-07-15 20:22:28 +0000

    set up migration and db entities

---
+++ app/models/project.py
@@ -0,0 +1,13 @@
+from app.models.mixin_models import TimestampMixin
+from app import db
+
+
+class Project(TimestampMixin, db.Model):
+
+    __tablename__ = "project"
+
+    id = db.Column(db.Integer, primary_key=True)
+    project_uuid = db.Column(db.String(36), nullable=False)
+    project_name = db.Column(db.Text, nullable=False)  # TODO make it string with fix max width
+    client_id = db.Column(db.Integer, nullable=False)
+    equipment_id = db.Column(db.Integer)
