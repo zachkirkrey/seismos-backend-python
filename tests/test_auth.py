7170a9c8fb082e910c76efb74d6cd76d1d5768c8
zachkirkrey <zachkirkrey@gmail.com>
2021-07-16 10:22:31 +0000

    testing auth endpoint

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,30 +1,37 @@
 import pytest
 
-from app import db, create_app
+from app import create_app
 
 
 @pytest.fixture
 def client():
-    app = create_app(environment="testing")
-    app.config["TESTING"] = True
+    app = create_app()
+    app.config['TESTING'] = True
 
-    with app.test_client() as client:
-        app_ctx = app.app_context()
-        app_ctx.push()
-        db.drop_all()
-        db.create_all()
-        yield client
-        db.session.remove()
-        db.drop_all()
-        app_ctx.pop()
+    with app.app_context():
+        with app.test_client() as client:
+            yield client
 
 
-def test_db_create_user(client):
-    from app.models import User
-    username, passwd = "imgroot", "1234"
-    user = User(username=username, password=passwd, email="imgroot@gmail.com")
-    user.save()
+def test_auth(client):
+    creds = {
+        "username": "bobo",
+        "password": "1234",
+    }
 
-    user = User.query.filter(User.username == username).first()
-    assert user
-    assert user.username == username
+    resp = client.post("/api/auth", json=creds)
+    assert resp
+    assert resp.status_code == 200
+    assert resp.json
+    assert resp.json["status"] == 200
+    assert resp.json["message"] == "Login Successful"
+    assert resp.json["data"]["access_token"]
+
+    access_token = resp.json["data"]["access_token"]
+    resp = client.get("/api/auth", headers={"Authorization": f"Bearer {access_token}"})
+    assert resp
+    assert resp.status_code == 200
+    assert resp.json
+    assert resp.json["status"] == 200
+    assert resp.json["message"] == "User details"
+    assert resp.json["data"]["user"]
