2baa567242d27384f34ae8a85a9d65e59bcdcb6b
zachkirkrey <zachkirkrey@gmail.com>
2021-07-23 16:10:36 +0000

    add project -> equipment relation

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
 import json
 from .utils import USER_CREDS
+from app.models import Project
 
 
 def test_auth(client_with_user):
@@ -42,3 +43,22 @@ def test_project_endpoint(client_with_user):
         assert resp.json["status"] == 200
         assert resp.json["data"]
         assert resp.json["data"]["project"]
+
+        project = Project.query.filter(
+            Project.id == resp.json["data"]["project"]["id"]
+        ).first()
+
+        # project data test
+        assert project
+        assert project.project_name == payload["projectValues"]["project_name"]
+        assert project.project_uuid == payload["projectValues"]["project_uuid"]
+
+        # equipment test
+        assert project.equipment
+        assert project.equipment.trailer_id == payload["equipmentValues"]["trailers_id"]
+        assert project.equipment.powerpack_id == payload["equipmentValues"]["powerpack_id"]
+        assert project.equipment.source_id == payload["equipmentValues"]["source_id"]
+        assert project.equipment.accumulator_id == payload["equipmentValues"]["accumulator_id"]
+        assert project.equipment.hydrophones_id == payload["equipmentValues"]["hydrophones_id"]
+        assert project.equipment.transducer_id == payload["equipmentValues"]["transducer_id"]
+        assert project.equipment.hotspot_id == payload["equipmentValues"]["hotspot_id"]
