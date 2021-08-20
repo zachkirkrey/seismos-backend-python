c263daa1553f3929df4acddae2d0fa1b6d832bed
nixissmtp <gleb@simple2b.(none)>
2021-08-20 15:22:18 +0000

    remove client reduntant fields

--- app/models/client.py
+++ app/models/client.py
@@ -6,14 +6,12 @@ class Client(TimestampMixin, db.Model, ModelMixin):
 
     __tablename__ = "client"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     client_uuid = db.Column(db.String(36), nullable=False)
     client_name = db.Column(db.Text)
-    customer_field_rep_id = db.Column(db.Integer, nullable=False)
-    project_id = db.Column(db.Integer, nullable=False)
+    customer_field_rep_id = db.Column(db.Text)
+    project_id = db.Column(db.BigInteger, nullable=True)
     operator_name = db.Column(db.Text)
     service_company_name = db.Column(db.Text)
     wireline_company = db.Column(db.Text)
-    other_comments = db.Column(db.String(200))
-    password = db.Column(db.String(50))
-    title = db.Column(db.String(50))
+    other_comments = db.Column(db.Text)
