b93d1ae8c4c74414af86d4c60ff7b94159dab75b
zachkirkrey <zachkirkrey@gmail.com>
2022-03-13 10:21:29 +0800

    fix: user authentication error under wrong password field length

--- app/models/user.py
+++ app/models/user.py
@@ -14,7 +14,7 @@ class User(db.Model, ModelMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(150), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     user_uuid = db.Column(db.String(36), default=uuid_string)
 
